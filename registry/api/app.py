cef44cc448a5208e51cd91cb9065fcec22197683
Charles OuGuo <shaldengeki@gmail.com>
2022-07-28 03:24:48 +0000

    Add additional endpoints

--- registry/api/app.py
+++ registry/api/app.py
@@ -26,6 +26,50 @@ def delete_subject(subject_name: str) -> str:
 
     return versions
 
+@app.route("/subjects/<subject_name>", methods=['POST'])
+def check_subject_schema(subject_name: str) -> str:
+    subject = models.Subject.query.filter(models.Subject.name == subject_name).first()
+    if subject is None:
+        abort(404)
+
+    data = request.get_json()
+
+    if "schema" not in data:
+        abort(400)
+
+    schema = data["schema"]
+    schema_type_name: str = data.get("schemaType", "AVRO")
+    if schema_type_name not in models.SchemaType:
+        abort(400)
+
+    schema_type = models.SchemaType[schema_type_name]
+
+    references: list[dict] = data.get("references", [])
+    reference_names = set([f"{reference['subject']}/{reference['version']}" for reference in references])
+
+    version = models.SubjectVersion.query\
+        .filter(
+            models.SubjectVersion.schema == schema and \
+            models.SubjectVersion.schema_type == schema_type
+        )\
+        .first()
+
+    if version is None:
+        abort(404)
+
+    version_reference_names = set([
+        f"{reference['subject']}/{reference['version']}" for reference in version.references
+    ])
+    if reference_names != version_reference_names:
+        abort(404)
+
+    return json.dumps({
+        "subject": version.subject.name,
+        "id": version.id,
+        "version": version.version_id,
+        "schema": version.schema,
+    })
+
 @app.route("/subjects/<subject_name>/versions")
 def subject_versions(subject_name: str) -> str:
     subject = models.Subject.query.filter(models.Subject.name == subject_name).first()
@@ -63,6 +107,8 @@ def create_subject_version(subject_name: str) -> str:
             f"{models.Subject.name}/{models.SubjectVersion.version_id}" in reference_names
         )\
         .all()
+    if len(reference_versions) != len(references):
+        abort(400)
 
     next_version = max(version.version_id for version in subject.versions) + 1
 
@@ -101,6 +147,24 @@ def subject_version(subject_name: str, version_id: int) -> str:
         "schemaType": version.schema_type.name,
         "schema": version.schema,
     })
+
+@app.route("/subjects/<subject_name>/versions/<int:version_id>/referencedby")
+def subject_version_referencedby(subject_name: str, version_id: int) -> str:
+    version = models.SubjectVersion.query\
+        .join(models.Subject, models.SubjectVersion.subject_id == models.Subject.id)\
+        .filter(
+            models.Subject.name == subject_name and \
+            models.SubjectVersion.version_id == version_id
+        )\
+        .first()
+
+    if version is None:
+        abort(404)
+
+    return json.dumps([
+        referer.id for referer in version.referrers
+    ])
+
 
 @app.route("/subjects/<subject_name>/versions/<int:version_id>/schema")
 def subject_version_schema(subject_name: str, version_id: int) -> str:
